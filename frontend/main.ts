41c37d0b6b5ffc264f637dc086cabb9b51bfbda4
Jonas Spies <jonas.spies@stud-mail.uni-wuerzburg.de>
2026-06-06 21:59:30 +0200

    Fixed logging in browser

--- frontend/main.ts
+++ frontend/main.ts
@@ -2,14 +2,16 @@ import { execute_file } from "../src/pipeline.js";
 
 function main(){
     const input = document.getElementById("pdfInput") as HTMLInputElement;
-
+    const logs : string[] = ["Initializing logs"]
     input.addEventListener("change", async () => {
         const file = input.files?.[0]
         if (!file)
             return
         const buffer = await file.arrayBuffer()
-        execute_file(buffer)
-    });
+        
+        execute_file(buffer,logs)
+        console.log(logs.join("\n"))
+    })
 }
 
 main()
